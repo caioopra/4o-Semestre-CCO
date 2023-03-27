0c6fc7d558ab8d85b7698ec95997e90a15b893bb
caioopra <caioprsilva@gmail.com>
2023-03-27 14:10:56 -0300

    ES: continuação semana2

--- paradigmas/2-python_multiparadigma/atividade2/2929-menor_pilha.py
+++ paradigmas/2-python_multiparadigma/atividade2/2929-menor_pilha.py
@@ -1,6 +1,7 @@
 N = int(input())
 
 pilha = []
+tamanho = 0
 
 while N != 0:
     operacao = input()
@@ -9,19 +10,23 @@ while N != 0:
         operacao, valor = operacao.split()
 
     if operacao == "MIN":
-        print(pilha[-1][1] if len(pilha) > 0 else "EMPTY")
+        print(pilha[-1][1] if tamanho > 0 else "EMPTY")
     elif operacao == "POP":
-        if len(pilha) == 0:
+        if not tamanho:
             print("EMPTY")
         else:
             pilha.pop()
+            tamanho -= 1
     else:
-        if len(pilha) == 0:
+        if not tamanho:
             pilha.append((int(valor), int(valor)))
+            tamanho += 1
         else:
             if int(valor) < pilha[-1][1]:
                 pilha.append((int(valor), int(valor)))
             else:
                 pilha.append((int(valor), pilha[-1][1]))
     
+            tamanho += 1
+
     N = N - 1
